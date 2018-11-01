5058567dfc7a4f3c84a892ea4bf4892d03ac97a3
Vyacheslav Boyko <mail4bvn@gmail.com>
2018-11-01 12:22:48 +0300

    hiding special chars for text highlightening

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -265,17 +265,6 @@
   :config
   (reverse-im-activate "russian-computer"))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (reverse-im which-key tangotango-theme powerline flatland-theme drag-stuff auto-complete anaconda-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; hide special chars for text highlightening
+(set 'org-hide-emphasis-markers t)
+
