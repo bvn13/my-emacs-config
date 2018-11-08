7dee8d9b05a8267a5efdb844cc538cf24088e5a0
Vyacheslav Boyko <mail4bvn@gmail.com>
2018-11-08 14:12:31 +0300

    added line highlightening for tables in org-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -272,6 +272,18 @@
 ;; hide special chars for text highlightening
 (set 'org-hide-emphasis-markers t)
 
+;; enable line highlight in org-mode tables
+;; from https://emacs.stackexchange.com/questions/29501/automatically-turn-on-hl-line-mode-inside-org-mode-tables
+(defun highlight-current-table-line ()
+  (interactive)
+  (if (org-at-table-p)
+      (hl-line-mode 1)
+    (hl-line-mode -1)))
+(defun setup-table-highlighting ()
+  (add-hook 'post-command-hook #'highlight-current-table-line nil t))
+(add-hook 'org-mode-hook #'setup-table-highlighting)
+(add-hook 'orgtbl-mode-hook #'setup-table-highlighting)
+
 
 ;; https://github.com/magnars/multiple-cursors.el
 (require 'multiple-cursors)
