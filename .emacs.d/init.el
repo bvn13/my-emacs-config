8f7b64b37a1cf2a8980bc0922b913b563e9917ee
Vyacheslav Boyko <mail4bvn@gmail.com>
2018-11-26 16:40:01 +0300

    added eport to ODT for testing

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -296,6 +296,15 @@
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
 
+;;export to odt
+(when (system-is-windows)
+  (if (file-exists-p "\"C:\\Program Files\\LibreOffice\\program\\soffice.exe\"")
+    (setq org-export-odt-convert-processes
+        "\"C:\\Program Files\\LibreOffice\\program\\soffice.exe\" --headless --convert-to %f%x --outdir %d %i"
+        org-export-odt-preferred-output-format "odt"
+        org-export-odt-convert-process "\"C:\\Program Files\\LibreOffice\\program\\soffice.exe\"")
+  (message "SOFFICE path not found")))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
